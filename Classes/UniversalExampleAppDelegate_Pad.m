07bbb9cce78d6bfa2f594a1115e5fd339405f566
Cory D. Wiles <kwylez@gmail.com>
2012-05-12 15:55:52 -0400

    creating the uisplitviewcontroller programmatically

--- Classes/UniversalExampleAppDelegate_Pad.m
+++ Classes/UniversalExampleAppDelegate_Pad.m
@@ -7,7 +7,8 @@
   //
 
 #import "UniversalExampleAppDelegate_Pad.h"
-#import "RootViewController.h"
+#import "RootViewController_Pad.h"
+#import "DetailViewController_Pad.h"
 
 @implementation UniversalExampleAppDelegate_Pad
 
@@ -17,63 +18,49 @@
 #pragma mark -
 #pragma mark Application lifecycle
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  
+  RootViewController_Pad *rootVC     = [[RootViewController_Pad alloc] initWithStyle:UITableViewStylePlain];
 
-  [window addSubview:splitViewController.view];
-  [window makeKeyAndVisible];
+  UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:rootVC];
+  
+  DetailViewController_Pad *detailVC = [[DetailViewController_Pad alloc] init];
+  
+  self.splitViewController = [[UISplitViewController alloc] init];
+  
+  self.splitViewController.delegate        = detailVC;
+  self.splitViewController.viewControllers = [NSArray arrayWithObjects:navController, detailVC, nil];
+  
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  
+  self.window.rootViewController = self.splitViewController;
+  
+  [self.window makeKeyAndVisible];
   
   return YES;
 }
 
-
 - (void)applicationWillResignActive:(UIApplication *)application {
-  /*
-   Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-   Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-   */
+  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
-
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-  /*
-   Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-   If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-   */
+  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
-
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-  /*
-   Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
-   */
+  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
-
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-  /*
-   Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-   */
+  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-  /*
-   Called when the application is about to terminate.
-   See also applicationDidEnterBackground:.
-   */
+  
 }
-
-
-#pragma mark -
-#pragma mark Memory management
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-  /*
-   Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
-   */
-}
-
-
 
 
 @end
